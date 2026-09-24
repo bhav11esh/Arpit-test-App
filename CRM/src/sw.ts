126f6b7a01956ad5b77766408f78659c814d13b9
Arpit-Mudgal <54657570+Arpit-Mudgal@users.noreply.github.com>
2026-09-25 01:12:12 +0530

    fix: add skipWaiting and clientsClaim to sw.ts for immediate PWA update activation

--- CRM/src/sw.ts
+++ CRM/src/sw.ts
@@ -1,7 +1,11 @@
 /// <reference lib="webworker" />
 import { precacheAndRoute } from 'workbox-precaching';
+import { clientsClaim } from 'workbox-core';
 
 declare let self: ServiceWorkerGlobalScope;
+
+self.skipWaiting();
+clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
